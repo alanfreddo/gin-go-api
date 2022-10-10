4bd75372adb4ad431e977e4599f4788751d8d6b0
alanfreddo <4S06iV0w>
2022-10-10 20:29:58 -0300

    1.2

--- main.go
+++ main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"github.com/alanfreddo/gin-go-api/database"
-	"github.com/alanfreddo/gin-go-api/models"
 	"github.com/alanfreddo/gin-go-api/routes"
 )
 
 func main() {
 	database.ConectaComBancoDeDados()
-	models.Alunos = []models.Aluno{
-		{Nome: "Alan", CPF: "000000000", RG: "9876347283723"},
-		{Nome: "Joya", CPF: "000000001", RG: "0000000000000"},
-	}
 	routes.HandleRequests()
 
 }
